ef58cb7d28c0ae234f86c25088a69ff585b1ab59
Vilkrin <contact@vilkrin.uk>
2025-03-20 17:15:02 +0000

    wip
    wip

--- database/migrations/2025_03_20_144838_create_permission_tables.php
+++ database/migrations/2025_03_20_144838_create_permission_tables.php
@@ -66,13 +66,16 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                $table->primary(
+                    [$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary'
+                );
             } else {
-                $table->primary([$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                $table->primary(
+                    [$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary'
+                );
             }
-
         });
 
         Schema::create($tableNames['model_has_roles'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole, $teams) {
@@ -90,11 +93,15 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+                $table->primary(
+                    [$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary'
+                );
             } else {
-                $table->primary([$pivotRole, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+                $table->primary(
+                    [$pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary'
+                );
             }
         });
 
@@ -136,5 +143,39 @@ return new class extends Migration
         Schema::drop($tableNames['model_has_permissions']);
         Schema::drop($tableNames['roles']);
         Schema::drop($tableNames['permissions']);
+    }
+
+    // Role Seeding for Production.
+    public function seed(): void // <== not sure if this is correct or i need to rename it. 
+    {
+        $permissionsByRole = [
+            'admin' => ['restore posts', 'force delete posts'],
+            'editor' => ['create a post', 'update a post', 'delete a post'],
+            'viewer' => ['view all posts', 'view a post']
+        ];
+
+        $insertPermissions = fn($role) => collect($permissionsByRole[$role])
+            ->map(fn($name) => DB::table('permissions')->insertGetId(['name' => $name, 'guard_name' => 'web']))
+            ->toArray();
+
+        $permissionIdsByRole = [
+            'admin' => $insertPermissions('admin'),
+            'editor' => $insertPermissions('editor'),
+            'viewer' => $insertPermissions('viewer')
+        ];
+
+        foreach ($permissionIdsByRole as $role => $permissionIds) {
+            $role = Role::whereName($role)->first();
+
+            DB::table('role_has_permissions')
+                ->insert(
+                    collect($permissionIds)->map(fn($id) => [
+                        'role_id' => $role->id,
+                        'permission_id' => $id
+                    ])->toArray()
+                );
+        }
+        // need to add the below comment at some point.
+        // and also add the command to flush the cache again now after doing all these inserts
     }
 };
